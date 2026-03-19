3b84e070de238c3da0ac7f0a03659ded905f5f8d
Abricosovoe <95168716+Ibricate@users.noreply.github.com>
2026-03-19 14:14:38 +0200

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -12,9 +12,13 @@ int main()
         std::cout << "1. Добавить книгу\n";
         std::cout << "2. Показать все книги\n";
         std::cout << "3. Удалить по индексу\n";
+        std::cout << "4. Сохранить в файл\n";
+        std::cout << "5. Загрузить из файла\n";
         std::cout << "0. Выход\n";
         std::cout << "Выбор: ";
+
         std::cin >> choice;
+
         try
         {
             switch (choice)
@@ -22,11 +26,9 @@ int main()
             case 1:
                 lib.AddFromConsole();
                 break;
-
             case 2:
                 lib.PrintAll();
                 break;
-
             case 3:
             {
                 lib.PrintAll();
@@ -35,6 +37,23 @@ int main()
                 if (!(std::cin >> idx))
                     throw std::runtime_error("Ошибка ввода: ожидалось целое число.");
                 lib.DeleteIndex(idx);
+                break;
+            }
+            case 4:
+            {
+                char filename[255];
+                std::cout << "Введите имя файла для сохранения: ";
+                std::cin >> filename;
+                lib.SaveToFile(filename);
+                break;
+            }
+
+            case 5:
+            {
+                char filename[255];
+                std::cout << "Введите имя файла для загрузки: ";
+                std::cin >> filename;
+                lib.LoadFromFile(filename);
                 break;
             }
             case 0:
@@ -48,6 +67,8 @@ int main()
         catch (const std::exception& e)
         {
             std::cout << "Исключение: " << e.what() << std::endl;
+            std::cin.clear();
+            std::cin.ignore(10000, '\n');
         }
     } while (choice != 0);
 }
